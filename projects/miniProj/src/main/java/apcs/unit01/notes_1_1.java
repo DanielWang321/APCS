119f4d137cb095231d051c0d601f436935af96af
Daniel Wang <danielruoqiao@gmail.com>
2025-08-25 11:27:24 -0400

    added notes

---
+++ projects/miniProj/src/main/java/apcs/unit01/notes_1_1.java
@@ -0,0 +1,54 @@
+package apcs.unit01;
+
+/*
+Topics to cover:
+basic program layout
+comments
+class header
+main method header
+naming rules and conventions
+print vs. println
+escape characters
+identifying errors - types of errors
+predict the output
+fix me
+*/
+//this is a line comment
+//identifier: any thing that you are naming (Class, variable, function/method, constant)
+
+/*
+    1. Can contain underscores, letters, digits, $
+    2. Can begin with underscores, letters, $
+    3. Cannot be a keyword (reserved word)
+
+    Naming concentions:
+    1. camelcase
+    2. class names begin with capital letter
+    3. function & variable names should begin with lowercase
+    4. constants (enums) in all caps
+    5. identifier names should be meaningful
+ */
+public class notes_1_1 //class header
+{ 
+    //public: access modifier, accessed by other class
+    //class: type we are creating
+    //notes_1_1 is name of class
+
+}//braces define class body
+
+/*
+predict the output:
+Example 1:
+System.out.println("This\tis a \"crazy\"\nline/nof text\\!");
+Example 2:
+System.out.print("\"Welcome//to APCS!\nLet's have a great year!\""
+System.out.println(" -Said someone :\\")
+fix me:
+public class Note_1_1
+{
+public static vod main(String[ args)
+{
+system.out.printl(Hello World!);
+system.out.pint("Welcome to APCS :)")
+}
+*/
