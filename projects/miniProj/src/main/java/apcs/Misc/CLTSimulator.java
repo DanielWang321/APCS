157e5272ba33163a9e9443b578ee6d7fc8e98b2e
Daniel Wang <danielruoqiao@gmail.com>
2026-01-08 17:34:00 -0500

    added mean + sd

--- projects/miniProj/src/main/java/apcs/Misc/CLTSimulator.java
+++ projects/miniProj/src/main/java/apcs/Misc/CLTSimulator.java
@@ -9,6 +9,10 @@ public class CLTSimulator {
         Map<String, Integer> simulationResults = new LinkedHashMap<>();
         int[] simulationArray = new int[20];
         double[] ArrayIndexes = new double[20];
+        double sum = 0;
+        double average = 0;
+        double standardDeviation = 0;
+        double sumOfSquares = 0;
         for (int repititions = 0; repititions < 1000; repititions++) {
             int successfulGuesses = 0;
             for (int trials = 0; trials < 30; trials++) {
@@ -16,10 +20,12 @@ public class CLTSimulator {
                     successfulGuesses += 1;
                 }
             }
-            System.out.println(successfulGuesses);
+            //System.out.println(successfulGuesses);
             double proportion = successfulGuesses / 30.0;
+            sum += proportion;
+            sumOfSquares += proportion * proportion;
             simulationArray[(int)(proportion/0.05)] = simulationArray[(int)(proportion/0.05)] +1;
-            System.out.println("added one to " + (int)(proportion/0.05));
+            //System.out.println("added one to " + (int)(proportion/0.05));
             
         }
         for (int i = 0; i < 20; i++) {
@@ -35,5 +41,12 @@ public class CLTSimulator {
         for (Map.Entry<String, Integer> entry : simulationResults.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+        average = sum/1000;
+        System.out.println("Average: " + (average));
+
+        double meanSquare = sumOfSquares/1000.0;
+        standardDeviation = Math.sqrt(meanSquare - (average * average));
+        System.out.println("Standard Deviation: " + standardDeviation);
+
     }
 }
